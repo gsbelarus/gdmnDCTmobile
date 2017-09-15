5303feb87a6146d6834451e1ed2f2f1f369963da
sywka1 <sywka1@gmail.com>
2017-09-15 17:37:34 +0300

    moment workaround

--- src/localization/utils.js
+++ src/localization/utils.js
@@ -4,5 +4,11 @@ import moment from 'moment'
 import { getLocale } from './strings'
 
 export function formatDate (date, format, locale) {
-  return moment(date).locale(locale || getLocale()).format(format)
+  locale = locale || getLocale()
+  switch (locale) {
+    case 'ru-RU':
+      locale = 'ru'
+      break
+  }
+  return moment(date).locale(locale).format(format)
 }
